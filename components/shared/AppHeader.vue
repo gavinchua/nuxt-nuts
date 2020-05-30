9a22ef66368c6183b531f4516fb0265cb267dcea
Gavin Chua <me@gavinchua.com>
2020-05-31 00:49:18 +0800

    add - header & footer

---
+++ components/shared/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="header">
+    <h1 class="title">
+      header
+    </h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+};
+</script>
+
+<style lang="scss">
+
+</style>
